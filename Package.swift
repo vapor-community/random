9d9348c49bb794d90dd9d1e4baf1433641561125
Logan Wright <logan.william.wright@gmail.com>
2017-03-27 12:38:03 +0100

    beta dependencies

--- Package.swift
+++ Package.swift
@@ -6,6 +6,6 @@ let package = Package(
     name: "Random",
     dependencies: [
         // Core extensions, type-aliases, and functions that facilitate common tasks.
-        .Package(url: "https://github.com/vapor/core.git", Version(2,0,0, prereleaseIdentifiers: ["alpha"]))
+        .Package(url: "https://github.com/vapor/core.git", Version(2,0,0, prereleaseIdentifiers: ["beta"]))
     ]
 )
