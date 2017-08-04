ccf692fc37aca704bdcc73261b5faec9d1804268
Logan Wright <loganwright@the-neutral-zone.local>
2017-08-04 12:21:24 +0200

    swift 4 package manifest

---
+++ Package@swift-4.swift
@@ -0,0 +1,17 @@
+// swift-tools-version:4.0
+import PackageDescription
+
+let package = Package(
+    name: "Random",
+    products: [
+        .library(name: "Random", targets: ["Random"]),
+    ],
+    dependencies: [
+        // Core extensions, type-aliases, and functions that facilitate common tasks.
+        .package(url: "https://github.com/vapor/core.git", from: "2.0.0"),
+    ],
+    targets: [
+        .target(name: "Random", dependencies: ["Core"]),
+        .testTarget(name: "RandomTests", dependencies: ["Random"]),
+    ]
+)
